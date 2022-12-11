071291acbb93419283e026c4548aab024c4f3312
Maryfrances Umeora <mumeora@u.rochester.edu>
2022-12-11 16:31:26 -0600

    basename is necessary

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter baseName="/Portfolio">
       <Routes>
 
         <Route path="/" element={<Home />} />
