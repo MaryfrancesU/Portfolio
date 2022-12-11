7bfba93077e4eb04d823ed98f7ab5d9162375116
Maryfrances Umeora <mumeora@u.rochester.edu>
2022-12-11 16:24:02 -0600

    remove basename from browserrouter

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename="/Portfolio">
+    <BrowserRouter basename="">
       <Routes>
 
         <Route path="/" element={<Home />} />
