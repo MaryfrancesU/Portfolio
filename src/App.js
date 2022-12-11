2158f8f3b819967654bea8304e690fd849922b5a
Maryfrances Umeora <mumeora@u.rochester.edu>
2022-12-11 16:14:38 -0600

    trying to fix ghpages routing issue

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Portfolio">
       <Routes>
 
         <Route path="/" element={<Home />} />
