2f44609b84e642f77b21e3ddcffb6f8b5d3c38e1
Maryfrances Umeora <mumeora@u.rochester.edu>
2022-12-12 19:24:50 -0600

    update mobile road order

--- src/Home/Journey/MobileRoad/MobileRoad.js
+++ src/Home/Journey/MobileRoad/MobileRoad.js
@@ -6,20 +6,19 @@ const MobileRoad = () => {
         <table id="mobileTable">
 
             <tr>
-                <td>  </td>
+                <td> Stick Figure </td>
                 <td> 
                     <a href="/Portfolio/about"> <div className='circle'> About Me </div> </a>
                 </td>
-                <td> Stick Figure </td>
+                <td>  </td>
             </tr>
 
             <tr>
-                
+                <td> </td>
+                <td> </td>
                 <td> 
                     <a href="/Portfolio/projects"> <div className='circle'> Projects </div> </a>
                 </td>
-                <td> </td>
-                <td> </td>
             </tr>
 
             <tr>
@@ -30,14 +29,14 @@ const MobileRoad = () => {
                 <td> </td>
             </tr>
 
-            <tr>   
+            <tr> 
+                <td> </td>
+                <td> </td>  
                 <td> 
                     <div className='circle italics' style={{cursor: 'default'}}> 
                         Who knows <br/> what&apos;s next <br /> on my journey? 
                     </div>
-                </td>
-                <td> </td>
-                <td> </td>
+                </td>  
             </tr>         
 
         </table>
