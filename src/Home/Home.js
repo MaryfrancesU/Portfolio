f036a538d02ef2e9e42d966dd1f8c9fd1aef3c82
Maryfrances Umeora <mumeora@u.rochester.edu>
2023-08-20 15:21:19 -0500

    update homepage design

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,6 @@
 import './Home.scss';
 import React from 'react';
-import Journey from './Journey/Journey';
+// import Journey from './Journey/Journey';
 import NavBar from '../shared/NavBar';
 
 const Home = () => {
@@ -8,11 +8,11 @@ const Home = () => {
   return (
     <>
         <NavBar currentPage='home' />
-        <div className='notice'> 
+        {/* <div className='notice'> 
             <span> NOTE!! </span>
             This page is undergoing a makeover (which is why dark mode looks so awful right now). 
             Check out the other pages for now! 
-        </div>
+        </div> */}
 
         <div className='container'>
             <div className='quote'>
@@ -21,7 +21,32 @@ const Home = () => {
                 <p className="regtxt"> and I believe that </p>
                 <p id="learning"> &ldquo;Learning is a lifelong journey.&rdquo; </p>
             </div>
-            <Journey />
+            {/* <Journey /> */}
+
+            <div className='links'>
+
+                <a className="link-outline" href='/Portfolio/about'> 
+                    <div className="link-cover">
+                        ABOUT ME
+                    </div>
+                    <span className="link-desc"> Click to learn more about me! </span>
+                </a>
+
+                <a className="link-outline" href='/Portfolio/projects'> 
+                    <div className="link-cover">
+                        MY PROJECTS
+                    </div>
+                    <span className="link-desc"> Click for some projects I&apos;ve made! </span>
+                </a>
+
+                <a className="link-outline" href='/Portfolio/experience'> 
+                    <div className="link-cover">
+                        MY EXPERIENCE 
+                    </div>
+                    <span className="link-desc"> Click to see my previous jobs! </span>
+                </a>
+
+            </div>
         </div>
     </>
   )
